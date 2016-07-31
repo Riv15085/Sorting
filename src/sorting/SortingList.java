3f0a759747fdb8f7060f3fdd2db228623014c76b
jpcahueque <jpcahueque@gmail.com>
2016-07-31 01:21:55 -0600

    Update SortingList.java

--- src/sorting/SortingList.java
+++ src/sorting/SortingList.java
@@ -19,6 +19,7 @@ public class SortingList {
         String archivo="C:\\Users\\Usuario\\Documents\\NetBeansProjects\\SortingList\\src\\sortinglist\\prueba.txt";
         
         Vector parrafo = Lector.leerArchivo(archivo);
+        Integer[] listaArray = new Integer[parrafo.size()];
         
         //se accede al elemento del vector 
         for (int i = 0; i<parrafo.size();i++){
@@ -26,8 +27,11 @@ public class SortingList {
             //se castea a int y se agrega a la lista
             num=Integer.parseInt(elemento);
             miLista.addNodo(num);
+            listaArray[i] = (Integer)num;
         }
         
+        
+        //miLista.mergeSort(listaArray); /*Se utiliza mergeSort*/
         //se utiliza seleccion sort
         miLista.selectionSort();
         miLista.Print();
